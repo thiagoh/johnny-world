b92b87a8a0fb100581ee7ee1886b8ac30eb25f96
Thiago Andrade <thiagoh@gmail.com>
2016-10-14 08:08:54 -0400

    johnny with music

---
+++ Assets/_Scripts/DamagePlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DamagePlayerScript : MonoBehaviour {
+
+    private PlayerController controller;
+
+    public void Start() {
+
+        controller = GameObject.FindObjectOfType<PlayerController>();
+    }
+
+    private void damage(GameObject gameObject) {
+        controller.damage();
+    }
+
+    public void OnCollisionEnter2D(Collision2D other) {
+
+        if (other.gameObject.CompareTag("Player")) {
+            damage(other.gameObject);
+        }
+    }
+
+    public void OnCollisionStay2D(Collision2D other) {
+
+        if (other.gameObject.CompareTag("Player")) {
+            damage(other.gameObject);
+        }
+    }
+
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,8 +4,9 @@ using System;
 
 public class PlayerController : MonoBehaviour {
 
-    public float velocity = 10f;
+    public float velocity = 30f;
     public float jumpForce = 100f;
+    public int rings = 0;
     public Camera camera;
     public Transform spawnPoint;
 
@@ -15,6 +16,9 @@ public class PlayerController : MonoBehaviour {
     private float jump;
     private bool facingRight;
     private bool grounded;
+    private int movingTo;
+
+    private static float MAX_VELOCITY = 5f;
 
     [Header("Sound Clips")]
     public AudioSource jumpSound;
@@ -23,6 +27,16 @@ public class PlayerController : MonoBehaviour {
     // Use this for initialization
     void Start() {
         initialize();
+    }
+
+    public void damage() {
+
+        if (rings > 0) {
+            rings = 0;
+        } else if (rings <= 0) {
+            rings = 0;
+            die();
+        }
     }
 
     private void initialize() {
@@ -37,10 +51,10 @@ public class PlayerController : MonoBehaviour {
 
     public void FixedUpdate() {
 
-        float axis = Input.GetAxis("Horizontal");
+        float axis = Input.GetAxisRaw("Horizontal");
         move = normalize(axis);
 
-        Debug.Log("Move: " + axis + " || " + move + " Grounded: " + grounded);
+        //Debug.Log("Move: " + axis + " || " + move + " Grounded: " + grounded);
 
         if (move > 0f) {
             facingRight = true;
@@ -54,18 +68,43 @@ public class PlayerController : MonoBehaviour {
 
         if (grounded) {
 
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetAxis("Vertical") > 0f) {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) {
                 jump = 1f;
                 //jumpSound.Play();
+                movingTo = rigidbody.velocity.x == 0f ? 0 : (int)(rigidbody.velocity.x / Math.Abs(rigidbody.velocity.x));
             }
+
         } else {
-            move = 0f;
+            //move = 0f;
         }
 
-        rigidbody.AddForce(new Vector2(
-                   move * velocity,
-                   jump * jumpForce),
-                   ForceMode2D.Force);
+        //print("velocity: " + rigidbody.velocity + " axis: " + axis);
+        int currentMovingTo = rigidbody.velocity.x == 0f ? 0 : (int)(rigidbody.velocity.x / Math.Abs(rigidbody.velocity.x));
+        if (grounded || currentMovingTo == movingTo) {
+            if ((rigidbody.velocity.x >= 1f && axis < 0f)
+                ||
+                (rigidbody.velocity.x <= -1f && axis > 0f)) {
+                // moving to one direction but trying to break (move to another direction)
+                move = Mathf.Clamp(-rigidbody.velocity.x * 0.8f, -MAX_VELOCITY, MAX_VELOCITY);
+                print("BREAK! Move to " + (move > 0f ? "right" : "left") + " " + move);
+            }
+        }
+
+        if (move != 0f) {
+            print("Vel: " + rigidbody.velocity.x + " move: " + move);
+        }
+
+        //print("is between: " + vel: 3.788259 move: -3.030607)
+        //print("is between: " + (isBetween(3.788259f, -MAX_VELOCITY, 1f) ? "yes" : "no"));
+
+        if ((move > 0f && rigidbody.velocity.x < MAX_VELOCITY)
+            ||
+            (move < 0f && rigidbody.velocity.x > -MAX_VELOCITY)) {
+            print("Apply!");
+            rigidbody.AddForce(new Vector2(move * velocity, 0f), ForceMode2D.Force);
+        }
+
+        rigidbody.AddForce(new Vector2(0f, jump * jumpForce), ForceMode2D.Force);
 
         if (jump > 0f) {
             jump = 0f;
@@ -73,6 +112,17 @@ public class PlayerController : MonoBehaviour {
 
         moveCamera();
         Input.ResetInputAxes();
+    }
+
+    public static bool isBetween(float v, float from, float to) {
+
+        if (to < from) {
+            float lower = to;
+            to = from;
+            from = lower;
+        }
+
+        return v >= from && v <= to;
     }
 
     private void flip() {
